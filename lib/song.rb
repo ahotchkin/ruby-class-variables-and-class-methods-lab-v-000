9934e802fc0e012c1db4c1462c69b362324a8a1e
Allyson Hotchkin <ahotchk1@yahoo.com>
2018-03-25 20:35:18 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -36,7 +36,7 @@ class Song
   end
 
   def self.genre_count
-    genre_count = Hash.new 0
+    genre_count = Hash.new(0)
     @@genres.each do |genre|
       genre_count[genre] += 1
     end
@@ -44,7 +44,7 @@ class Song
   end
 
   def self.artist_count
-    artist_count = Hash.new 0
+    artist_count = Hash.new(0)
     @@artists.each do |artist|
       artist_count[artist] += 1
     end
